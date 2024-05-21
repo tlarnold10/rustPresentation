d8783dd12b3770ea99fb5192db60aab5f0709c65
tlarnold10 <tlarnold10@gmail.com>
2024-05-21 16:27:12 -0500

    adding parsing of story to grab place holders

--- client/storiesclient/src/main.rs
+++ client/storiesclient/src/main.rs
@@ -15,17 +15,17 @@ struct Story {
     fantasy: Vec<String>
 }
 
+struct Replacement {
+    originals: Vec<String>,
+    replacements: Vec<String>
+}
+
 fn print_variable_type<T>(_: &T) {
     println!("{}", std::any::type_name::<T>());
 }
 
-fn main() {
 
-    let mut input = String::new();
-    println!("Select a type of story: ");
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
-
+fn parse_input(input: &String) -> String {
     let mut story_genre = String::new();
     match input.to_uppercase().trim() {
         "ADVENTURE" => {story_genre = "adventure".to_string()},
@@ -35,6 +35,25 @@ fn main() {
         "FANTASY" => {story_genre = "fantasy".to_string()},
         _ => panic!("Please enter a valid story genre: adventure, romcom, family, horror, or fantasy")
     };
+    return story_genre;
+}
+
+fn vec_to_string(items: &[String]) -> () {
+    let items_iterator = items.iter();
+    for item in items_iterator {
+        println!("{}", item);
+    }
+}
+
+fn main() {
+
+    let mut input = String::new();
+    println!("Select a type of story (adventure, romcom, family, horror, fantasy): ");
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    let mut story_genre = String::new();
+    story_genre = parse_input(&input);
 
     println!("{}", story_genre);
     // first we need to get the data from the server
@@ -42,14 +61,7 @@ fn main() {
         Ok(resp) => resp.text().unwrap(),
         Err(err) => panic!("Error: {}", err)
     };
-    // let data: Story = match serde_json::from_str(&resp) {
-    //     Ok(data) => data,
-    //     Err(err) => panic!("Error: {}", err)
-    // };
-    // let data: Story = serde_json::from_str(resp.as_str())?;
-    // println!("{}", data.origin);
 
-//    let mut file = File::open("../data/stories.json");
     let mut file: File = match File::open("./data/stories.json") {
         Ok(file) => file,
         Err(err) => panic!("could not open: {}", err)
@@ -84,10 +96,23 @@ fn main() {
     // print_variable_type(&original_story);
     let pick_one = rand::thread_rng().gen_range(0..2);
     println!("random number: {}", pick_one);
-    let story_iter = chosen_stories.iter();
-    for story in story_iter {
-        println!("{}", story);
-    }
+    vec_to_string(&chosen_stories);
     let mut chosen_story = chosen_stories[pick_one].clone();
     println!("Random pick: {}", chosen_story);
+    let mut adding_chars = false;
+    let mut temp_item = String::new();
+    for character in chosen_story.chars() {
+        if (character == '<') {
+            adding_chars = true;
+        } else if (character == '>') {
+            adding_chars = false;
+            place_holders.push(temp_item);
+            temp_item = String::new();
+        } else if (adding_chars) {
+            temp_item.push(character);
+        }
+    }
+    vec_to_string(&place_holders);
+
 }
+
